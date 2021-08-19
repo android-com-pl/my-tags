0c0c3333774a33037b4409634123f30a2f6e2bf9
Rafał Całka <calka.rafal@gmail.com>
2021-08-20 01:39:30 +0200

    Fixed #1 and #2

--- src/LoadForumTagsRelationship.php
+++ src/LoadForumTagsRelationship.php
@@ -5,11 +5,13 @@ namespace Acpl\MyTags;
 use Flarum\Api\Controller\ShowForumController;
 use Flarum\Http\RequestUtil;
 use Flarum\Tags\Tag;
+use Illuminate\Database\Eloquent\Collection;
 use Psr\Http\Message\ServerRequestInterface;
 
 class LoadForumTagsRelationship
 {
     /**
+     * Load tags that a user is following
      * @param ShowForumController $controller
      * @param $data
      * @param ServerRequestInterface $request
@@ -18,14 +20,20 @@ class LoadForumTagsRelationship
     {
         $actor = RequestUtil::getActor($request);
 
-        $data['tags'] = Tag::query()
+        $followedTags = Tag::query()
             ->join('tag_user', function ($join) use ($actor) {
                 $join->on('tag_user.tag_id', '=', 'tags.id');
                 $join->on('tag_user.user_id', '=', resolve('db')->raw($actor->id));
                 $join->whereNotNull('tag_user.subscription');
             })
-            ->selectRaw('tags.*')
+            ->select('tags.*')
             ->whereVisibleTo($actor)
             ->get();
+
+        $tags = Collection::empty();
+        $tags = $tags->merge($data['tags']);
+        $tags = $tags->merge($followedTags);
+
+        $data['tags'] = $tags;
     }
 }
